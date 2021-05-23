353baa829b9c3b840cef178e370e5a12c22ebecd
victory <vickywane@gmail.com>
2021-05-23 22:13:07 +0100

    feat: built basic project

---
+++ src/components/card.svelte
@@ -0,0 +1,27 @@
+<script>
+    import "../../static/home.css"
+
+    export let data;
+</script>
+<div style="margin: 0 4rem;" >
+
+    <div class="align-center" >
+        <img alt={data.name}
+             class="img"
+             src={data.img_uri}/>
+    </div>
+    <br />
+    <h3> {data.name}</h3>
+    <p>
+        {data.description}
+     </p>
+    <br />
+
+    <div class={"align-center"}>
+        <button
+                class="custom-btn"
+        >
+            Read Biography
+        </button>
+    </div>
+</div>
---
+++ src/routes/home.svelte
@@ -0,0 +1,48 @@
+<script context="module">
+    import "../../static/home.css"
+    import Card from '../components/card.svelte'
+    import Banner from '../components/banner.svelte'
+    import * as Data from '../data.json'
+
+    export async function preload(page, session) {
+        return [
+            {
+                id: 1,
+                name: "Marry Lessor",
+                dob: "19-05-1975",
+                country: "England"
+            }
+        ]
+    }
+</script>
+
+<svelte:head>
+    <title>IWD 2021</title>
+</svelte:head>
+
+<div class="App" >
+    <Banner />
+
+    <div class="cards-body">
+
+        <ul class="cards">
+            {#each Data.heroes as heroine}
+                <li>
+                    <Card data={heroine}/>
+                </li>
+            {/each}
+        </ul>
+        <br />
+        <br />
+
+        <div class="align-center">
+            <button
+                    class="custom-btn"
+                    style="background-color: transparent"
+            >
+                See More Heroines
+            </button>
+        </div>
+        <br />
+    </div>
+</div>
